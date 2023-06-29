cceb29df80fd1e07fdb37f889412dd21c2660c44
Ramom Gomes <ramomusica09@hotmail.com>
2023-06-28 21:22:37 -0300

    new-teste

--- leetcode.js
+++ leetcode.js
@@ -1,11 +1,7 @@
 function contador() {
     var numeros = [11,5,6];
 
-    if (numeros[1] + numeros[2] === numeros[0]) {
-        return true
-    } else {
-        return false
-    }
+    
 }
 
 console.log(contador());
