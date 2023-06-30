8a426932c68813cac612887a45c8444e41c2f7b6
Ramom Gomes <ramomusica09@hotmail.com>
2023-06-29 21:20:39 -0300

    vai

--- leetcode.js
+++ leetcode.js
@@ -1,23 +1,5 @@
-function contador(n1,n2) {
-    return n1 + n2;
-}
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
 
-
-console.log(contador(50,50));
-function contador(n1,n2) {
-    return n1 + n2;
-}
-
-console.log(contador(50,50));
-console.log(contador(50,50));
-function contador(n1,n2) {
-    return n1 + n2;
-}
-console.log(contador(50,50));
-function contador(n1,n2) {
-    return n1 + n2;
-}
-console.log(contador(50,50));
-function contador(n1,n2) {
-    return n1 + n2;
-}
+console.log(multiplica(50,100));
