1743f085071223dfcb02209ce1042bce1353d952
Ramom Gomes <ramomusica09@hotmail.com>
2023-06-29 21:17:27 -0300

    novo-algo

--- leetcode.js
+++ leetcode.js
@@ -3,3 +3,25 @@ function contador(n1,n2) {
 }
 
 console.log(contador(50,50));
+
+function contador(n1,n2) {
+    return n1 + n2;
+}
+
+console.log(contador(50,50));
+function contador(n1,n2) {
+    return n1 + n2;
+}
+
+console.log(contador(50,50));
+
+function contador(n1,n2) {
+    return n1 + n2;
+}
+
+console.log(contador(50,50));
+function contador(n1,n2) {
+    return n1 + n2;
+}
+
+console.log(contador(50,50));
