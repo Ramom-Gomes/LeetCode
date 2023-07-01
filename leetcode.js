71bc16871b0b16bd41d48bc5b48eb391fca1122e
Ramom Gomes <ramomusica09@hotmail.com>
2023-06-30 21:39:20 -0300

    so-assim

--- leetcode.js
+++ leetcode.js
@@ -2,47 +2,4 @@ function multiplica(n1, n2) {
     return n1 * n2;
 };
 
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-v
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
+console.log(multiplica(100,100));
