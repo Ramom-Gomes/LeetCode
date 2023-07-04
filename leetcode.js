9a9920ac7af3670645d55be0e7d5af9c58506a28
Ramom Gomes <ramomusica09@hotmail.com>
2023-07-03 21:48:44 -0300

    novo-teste

--- leetcode.js
+++ leetcode.js
@@ -24,3 +24,124 @@ onsole.log(multiplica(100,100));
 onsole.log(multiplica(100,100));
 
 onsole.log(multiplica(100,100));
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+
