34a0906d391bf1983f54134bb0bc1eee1d19b30a
Ramom Gomes <ramomusica09@hotmail.com>
2023-07-04 20:51:24 -0300

    nao

--- leetcode.js
+++ leetcode.js
@@ -2,27 +2,4 @@ function multiplica(n1, n2) {
     return n1 * n2;
 };
 
-
-
-
-console.log(multiplica(100,100));
-
-
-
-console.log(multiplica(100,100));
-
-
-
-console.log(multiplica(100,100));
-
-
-
-
-console.log(multiplica(100,100));
-
-
-console.log(multiplica(100,100));
-
-console.log(multiplica(100,100));
-
 console.log(multiplica(100,100));
