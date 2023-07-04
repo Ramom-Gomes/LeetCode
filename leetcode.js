4e76aa0a7683faff4cdd97f0a2e9bcd267a5114b
Ramom Gomes <ramomusica09@hotmail.com>
2023-07-03 21:47:43 -0300

    new

--- leetcode.js
+++ leetcode.js
@@ -5,27 +5,6 @@ function multiplica(n1, n2) {
 console.log(multiplica(100,100));
 
 
-
-
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-console.log(multiplica(100,100));
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-console.log(multiplica(100,100));
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
-console.log(multiplica(100,100));
-function multiplica(n1, n2) {
-    return n1 * n2;
-};
-
 console.log(multiplica(100,100));
 
 
@@ -34,3 +13,14 @@ function multiplica(n1, n2) {
 };
 
 console.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+onsole.log(multiplica(100,100));
+
+
+
+onsole.log(multiplica(100,100));
+
+onsole.log(multiplica(100,100));
