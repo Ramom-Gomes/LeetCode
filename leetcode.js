f2370a0f6b57ec4105eefd904c1525612356b1fa
Ramom Gomes <ramomusica09@hotmail.com>
2023-07-15 22:44:58 -0300

    sim

--- leetcode.js
+++ leetcode.js
@@ -4,3 +4,15 @@ function multiplica(n1, n2) {
 
 console.log(multiplica(100,100));
 
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+
