9bfd2a94d5f4abe73f5d7979f55e4dc1113f2c65
Ramom Gomes <ramomusica09@hotmail.com>
2023-07-18 21:03:28 -0300

    tabekeza

--- leetcode.js
+++ leetcode.js
@@ -5,4 +5,26 @@ function multiplica(n1, n2) {
 console.log(multiplica(100,100));
 
 
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+
+
+function multiplica(n1, n2) {
+    return n1 * n2;
+};
+
+console.log(multiplica(100,100));
+
+
+
 
