4fa8bc7c4a19f2a60adefde0becf6ac9842488d0
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-09-24 21:08:54 -0300

    sim

--- teste/src/App.jsx
+++ teste/src/App.jsx
@@ -1,48 +1,50 @@
 import React, { useEffect, useState } from 'react';
 
 function ExerciseList() {
-  const [exercises, setExercises] = useState([]);
-  const [exerciseImages, setExerciseImages] = useState({});
-
+  let [Vl, setVl] = useState("");
+  let [Cd, setCd] = useState("");
+  let [Qt, setQt] = useState("");
+  let [Resultado, setResultado] = useState("");
+  
   useEffect(() => {
-    const exerciseApiUrl = 'https://wger.de/api/v2/exercise/';
-    const imageApiUrl = 'https://wger.de/api/v2/exerciseimage/';
+    if(Cd == 1) {
+      setVl(4);
+    } if (Cd == 2) {
+      setVl(4.50);
+    } if (Cd == 3) {
+      setVl(5);
+    } if (Cd == 4) {
+      setVl(2);
+    } if (Cd == 5) {
+      setVl(1,50);
+    } else if (Cd > 5 || Cd < 1) {
+      setVl("Produto não existe");
+    };
+    setResultado(Vl * Qt);
+  }, [Cd, Qt])
 
-    // Buscar exercícios
-    fetch(exerciseApiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setExercises(data.results);
-
-        // Mapear IDs de exercícios para URLs de imagens
-        const imageUrls = {};
-        data.results.forEach((exercise) => {
-          if (exercise.images && exercise.images.length > 0) {
-            const imageId = exercise.images[0].exercise_base;
-            imageUrls[exercise.id] = `${imageApiUrl}${imageId}/`;
-          }
-        });
-        setExerciseImages(imageUrls);
-      })
-      .catch((error) => {
-        console.error('Erro ao buscar exercícios:', error);
-      });
-  }, []);
+  let Calcular  = () => {
+    if(Cd == 1) {
+      setVl(4);
+    } if (Cd == 2) {
+      setVl(4.50);
+    } if (Cd == 3) {
+      setVl(5);
+    } if (Cd == 4) {
+      setVl(2);
+    } if (Cd == 5) {
+      setVl(1,50);
+    } else if (Cd > 5 || Cd < 1) {
+      setVl("Produto não existe");
+    };
+  }
 
   return (
     <div>
-      <h1>Lista de Exercícios</h1>
-      <ul>
-        {exercises.map((exercise) => (
-          <li key={exercise.id}>
-            <h2>Nome: {exercise.name}</h2>
-            <p>Descrição: {exercise.description}</p>
-            {exerciseImages[exercise.id] && (
-              <img src={exerciseImages[exercise.id]} alt={`Imagem do exercício ${exercise.name}`} />
-            )}
-          </li>
-        ))}
-      </ul>
+      <input type="number" placeholder='codigo' onChange={e => (setCd(e.target.value)) } />
+      <input type="number" placeholder='quantidade' onChange={e => (setQt(e.target.value)) } />
+      <button onClick={Calcular}>Calcular</button>
+      <div>{Resultado}</div>
     </div>
   );
 }
