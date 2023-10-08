973e1ce331d3bafd00bdb0407c684d75a136d1d2
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-10-08 16:37:52 -0300

    lk

--- teste/src/App.jsx
+++ teste/src/App.jsx
@@ -1,50 +1,24 @@
-import React, { useEffect, useState } from 'react';
-
 function ExerciseList() {
-  let [Vl, setVl] = useState("");
-  let [Cd, setCd] = useState("");
-  let [Qt, setQt] = useState("");
-  let [Resultado, setResultado] = useState("");
-  
-  useEffect(() => {
-    if(Cd == 1) {
-      setVl(4);
-    } if (Cd == 2) {
-      setVl(4.50);
-    } if (Cd == 3) {
-      setVl(5);
-    } if (Cd == 4) {
-      setVl(2);
-    } if (Cd == 5) {
-      setVl(1,50);
-    } else if (Cd > 5 || Cd < 1) {
-      setVl("Produto não existe");
-    };
-    setResultado(Vl * Qt);
-  }, [Cd, Qt])
+  var r = ""
 
-  let Calcular  = () => {
-    if(Cd == 1) {
-      setVl(4);
-    } if (Cd == 2) {
-      setVl(4.50);
-    } if (Cd == 3) {
-      setVl(5);
-    } if (Cd == 4) {
-      setVl(2);
-    } if (Cd == 5) {
-      setVl(1,50);
-    } else if (Cd > 5 || Cd < 1) {
-      setVl("Produto não existe");
-    };
+  const solution = (str, ending, r) => {
+    // TODO: complete
+     srt = "abc";
+     ending = "bc"
+    const c = "d"
+    r = "r"
+    if(srt.slice(2) == ending) {
+      return r = "sim"
+    } else {
+      return r = "nao"
+    }
   }
+
+  console.log(r)
 
   return (
     <div>
-      <input type="number" placeholder='codigo' onChange={e => (setCd(e.target.value)) } />
-      <input type="number" placeholder='quantidade' onChange={e => (setQt(e.target.value)) } />
-      <button onClick={Calcular}>Calcular</button>
-      <div>{Resultado}</div>
+      ...
     </div>
   );
 }
